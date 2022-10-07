08ab8140bce19491a8999e1781b58f59d7a4c4ca
Christopher Persaud <nathanz@nycstudents.net>
2022-10-07 12:30:28 -0400

    calc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+// Getting the bill, tip percent, and number of people
+        System.out.println("Please enter the bill amount: ");
+        double bill = input.nextDouble();
+        System.out.println("Please enter a tip percentage: ");
+        int tip = input.nextInt();
+        System.out.println("Please enter the number of people: ");
+        int people = input.nextInt();
+
+        // Calculating the total tip amount, total bill including tip, tip per person, and total per person
+        TipCalculator j = new TipCalculator(people, tip);
+        j.addMeal(bill);
+        double totalTipAmount = j.tipAmount();
+        double totalBillIncludingTip = j.totalBill();
+        double tipPerPerson = j.perPersonTipAmount();
+        double totalPerPerson = j.perPersonTotalCost();
+
+        // Print the amounts
+        System.out.println("Total Tip Amount: " + totalTipAmount);
+        System.out.println("Total Bill Including Tip: " + totalBillIncludingTip);
+        System.out.println("Tip per person: " + tipPerPerson);
+        System.out.println("Total per person: " + totalPerPerson);
     }
 }
--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class TipCalculator {
+public class TipCalculator {
+
+    private int numPeople;
+    private int tipPercentage;
+    private double totalBillBeforeTip;
+
+    public TipCalculator(int numPeople, int tipPercentage){
+        this.numPeople = numPeople;
+        this.tipPercentage = tipPercentage;
+        totalBillBeforeTip = 0.0;
+    }
+
+    public double getTotalBillBeforeTip(){
+        return totalBillBeforeTip;
+    }
+
+    public double getTipPercentage(){
+        return tipPercentage;
+    }
+
+    public void addMeal(double cost){
+        totalBillBeforeTip =+ cost;
+    }
+
+    public double tipAmount(){
+        return (int) ((totalBillBeforeTip*(tipPercentage/100.0))*100.0)/100.0;
+    }
+
+    public double totalBill(){
+        return totalBillBeforeTip + tipAmount();
+    }
+
+    public double perPersonCostBeforeTip(){
+        return (int) ((totalBillBeforeTip/numPeople)*100)/100.0;
+    }
+
+    public double perPersonTipAmount(){
+        return (int) ((tipAmount()/numPeople)*100)/100.0;
+    }
+
+    public double perPersonTotalCost(){
+        return  ((totalBill()/numPeople)*100)/100.0;
+    }
 }
